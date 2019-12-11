9d22018ca8a6803db41ac06f0ec6321e7d6df9cf
Edgar Holzmann <e.holzmann.bvb@googlemail.com>
2019-12-11 10:16:43 +0000

    Create the class block with the origin position and its setter.

---
+++ src/kata7/Block.java
@@ -0,0 +1,15 @@
+package kata7;
+
+public class Block {
+    private double posX;
+    //double posY;
+
+    public Block(double posX) {
+        this.posX = posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+    
+}
